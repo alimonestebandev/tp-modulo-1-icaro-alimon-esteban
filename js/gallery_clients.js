cfa1a6f99b72633960ad215d2d245fa2a4308916
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-06-24 00:26:28 -0300

    agregue las funcionalidades a los botones y ademas cree la funcion para que se ilumine la imagen seleccionada

--- js/gallery_clients.js
+++ js/gallery_clients.js
@@ -1,8 +1,11 @@
+/* Imagen principal*/
 const principal_image = document.getElementById("principal-image");
-
+/* Contenedor de las imagenes en miniatura*/
 const mini_images_container = document.getElementById("mini-images-container");
 
-var index = 0;
+/*Botones */
+const button_prev = document.getElementById("button-prev");
+const button_next = document.getElementById("button-next");
 
 const imagesUrl = [
   "assetts/gallery_images/image_1.png",
@@ -12,44 +15,55 @@ const imagesUrl = [
   "assetts/gallery_images/image_5.png",
 ];
 
-var images = "";
-
-const setPrincipalImage = () => {
-  // main_image.src = url;
-  console.log("asd");
-};
+var index = 0;
 
 const next_image = () => {
-  if (index < imagesUrl.length) {
+  console.log("se clickeo next image");
+  if (index < imagesUrl.length - 1) {
     index++;
   } else {
     index = 0;
   }
   principal_image.src = imagesUrl[index];
+  update_state_opacity();
 };
 
 const prev_image = () => {
+  console.log("se clickeo prev image");
   if (index > 0) {
     index--;
   } else {
     index = imagesUrl.length - 1;
   }
   principal_image.src = imagesUrl[index];
+  update_state_opacity();
 };
+
+button_next.addEventListener("click", next_image);
+button_prev.addEventListener("click", prev_image);
 
 function select_image(e) {
   index = imagesUrl.indexOf(e);
   principal_image.src = e;
+  update_state_opacity();
 }
+
+const update_state_opacity = () => {
+  imagesUrl.map((images) => {
+    document.getElementById(images).style.opacity = "50%";
+  });
+  document.getElementById(imagesUrl[index]).style.opacity = "100%";
+};
 
 window.onload = () => {
   principal_image.src = imagesUrl[index];
-  prev_image();
+  var images = "";
   imagesUrl.map((e) => {
     images += `
     <div onclick='select_image("${e}")' class='overflow-hidden '>
-      <img draggable='false' class='mini-image' id='' src='${e}' alt='' />
+    <img draggable='false' class='mini-image' id=${e} src='${e}' alt='' />
     </div>`;
   });
   mini_images_container.innerHTML = images;
+  update_state_opacity();
 };
