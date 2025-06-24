ff931291490208324ea2fc3bb8da5c119defd784
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-06-23 23:49:22 -0300

    se agregan las imagenes de la carpeta assetts en el array de imagenes

--- js/gallery_clients.js
+++ js/gallery_clients.js
@@ -9,10 +9,11 @@ class image {
 const mini_images_container = document.getElementById("mini-images-container");
 
 const imagesUrl = [
-  "https://media.istockphoto.com/id/1323140489/es/foto/joven-florista-asi%C3%A1tico-confiado-propietario-de-una-peque%C3%B1a-tienda-de-flores-sosteniendo-la.jpg?s=612x612&w=0&k=20&c=Pu4GaLaOZDMHIwCWL_Y7SjgnWD4aoXlDeLdtz5aK6fk=",
-  "https://www.infomed.hlg.sld.cu/wp-content/uploads/2023/06/plantas-2.jpg",
-  "https://www.consumer.es/app/uploads/2004/03/Plantas-reaccionan-a-la-voz-1.jpg",
-  "https://uvn-brightspot.s3.amazonaws.com/assets/vixes/imj/vivirsalud/S/Sabias-que-hacer-jardineria-mejora-tu-salud-2.jpg",
+  "assetts/gallery_images/image_1.png",
+  "assetts/gallery_images/image_2.png",
+  "assetts/gallery_images/image_3.png",
+  "assetts/gallery_images/image_4.png",
+  "assetts/gallery_images/image_5.png",
 ];
 
 var images = "";
