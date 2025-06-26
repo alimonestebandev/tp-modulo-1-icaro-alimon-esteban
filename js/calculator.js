f8130126dbdccd936b1d141de2683cc1f9731a16
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-06-26 02:27:59 -0300

    arreglos en la calculadora, ahora funciona con los elementos html

--- js/calculator.js
+++ js/calculator.js
@@ -5,7 +5,7 @@ class calculate {
   }
 
   sum() {
-    return this.num1 + this.num2;
+    return parseInt(this.num1) + parseInt(this.num2);
   }
   subtract() {
     return this.num1 - this.num2;
@@ -18,23 +18,38 @@ class calculate {
   }
 }
 
-const numberOne = document.getElementById("number-one");
-const numberTwo = document.getElementById("number-two");
+const numberOne = document.getElementById("num-one");
+const numberTwo = document.getElementById("num-two");
 
 const finalValue = document.getElementById("final-value");
-
-var calc_nums = new calculate(numberOne, numberTwo);
+var calc_nums = new calculate(numberOne.value, numberTwo.value);
 
 const sum_btn = document.getElementById("sum-btn");
 const subtract_btn = document.getElementById("subtract-btn");
 const divide_btn = document.getElementById("divide-btn");
 const multiply_btn = document.getElementById("multiply-btn");
 
-sum_btn.addEventListener("click", set_final_value(calc_nums.sum()));
-subtract_btn.addEventListener("click", set_final_value(calc_nums.subtract()));
-divide_btn.addEventListener("click", set_final_value(calc_nums.divide()));
-multiply_btn.addEventListener("click", set_final_value(calc_nums.multiply()));
-
-const set_final_value = (newValue) => {
+sum_btn.addEventListener("click", () => {
+  calc_nums = new calculate(numberOne.value, numberTwo.value);
+  var newValue = calc_nums.sum();
   finalValue.value = newValue;
-};
+});
+subtract_btn.addEventListener("click", () => {
+  calc_nums = new calculate(numberOne.value, numberTwo.value);
+  var newValue = calc_nums.subtract();
+  finalValue.value = newValue;
+});
+divide_btn.addEventListener("click", () => {
+  calc_nums = new calculate(numberOne.value, numberTwo.value);
+  var newValue = calc_nums.divide();
+  finalValue.value = newValue;
+});
+multiply_btn.addEventListener("click", () => {
+  calc_nums = new calculate(numberOne.value, numberTwo.value);
+  var newValue = calc_nums.multiply();
+  finalValue.value = newValue;
+});
+
+//Prueba de funcionamiento --OK
+// var test_calc = new calculate(3, 3).subtract();
+// console.log(test_calc);
